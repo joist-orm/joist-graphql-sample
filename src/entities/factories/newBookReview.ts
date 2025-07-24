4468c7075f12ec60a7a4d981505e391c4f76ff01
Stephen Haberman <stephen.haberman@gmail.com>
2025-07-24 22:39:50 +0200

    Fix imports.

--- src/entities/factories/newBookReview.ts
+++ src/entities/factories/newBookReview.ts
@@ -1,6 +1,6 @@
 import { DeepNew, FactoryOpts, newTestInstance } from "joist-orm";
-import { BookReview } from "../entities";
-import type { EntityManager } from "../entities";
+import { BookReview } from "src/entities";
+import type { EntityManager } from "src/entities";
 
 export function newBookReview(em: EntityManager, opts: FactoryOpts<BookReview> = {}): DeepNew<BookReview> {
   return newTestInstance(em, BookReview, opts, {});
