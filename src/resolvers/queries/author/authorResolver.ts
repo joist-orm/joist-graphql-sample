55c63524784b730bb520be4f8e8185c3f1a560bc
Stephen Haberman <stephen.haberman@gmail.com>
2025-07-24 22:15:10 +0200

    Use load hint.

--- src/resolvers/queries/author/authorResolver.ts
+++ src/resolvers/queries/author/authorResolver.ts
@@ -1,7 +1,11 @@
 import { QueryResolvers } from "src/generated/graphql-types";
+import { Author } from "src/entities";
+import { convertInfoToLoadHint } from "joist-graphql-resolver-utils";
+import { getMetadata } from "joist-orm";
 
 export const author: Pick<QueryResolvers, "author"> = {
-  async author(root, args, ctx) {
-    throw new Error("not implemented");
+  async author(_, args, ctx, info) {
+    const hint = convertInfoToLoadHint(getMetadata(Author), info);
+    return ctx.em.load(Author, args.id, hint ?? {});
   },
 };
